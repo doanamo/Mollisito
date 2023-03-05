66b5d4e9ffa1591075378ad52ec7ca6395cd73de
Piotr Doan <doanpiotr@gmail.com>
2023-03-05 13:16:25 +0100

    :sparkles: Color constants

--- Source/Math/Constants.hpp
+++ Source/Math/Constants.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include "Vector.hpp"
+
 namespace Math
 {
     constexpr float Pi = 3.14159265359f;
+}
+
+namespace Color
+{
+    constexpr Math::Vector4f White = Math::Vector4f(1.0f, 1.0f, 1.0f, 1.0f);
+    constexpr Math::Vector4f Black = Math::Vector4f(0.0f, 0.0f, 0.0f, 1.0f);
 }
